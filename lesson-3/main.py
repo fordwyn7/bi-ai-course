5bba770eee323be1a18f0ca8f347d0ade225f73d
iqboljon7 <rsfordwyn@gmail.com>
2025-05-11 16:35:30 +0500

    homework done

--- lesson-3/main.py
+++ lesson-3/main.py
@@ -3,8 +3,8 @@
 # dir_path = r"D:\BI_AI_course\bi-ai-course\lesson-3\homework"
 
 # os.makedirs(dir_path, exist_ok=True)
-# for i in range(1, 24):
-#     file_name = f"set{i}.py"
+# for i in range(1, 26):
+#     file_name = f"dictionary{i}.py"
 #     file_path = os.path.join(dir_path, file_name)
 #     with open(file_path, 'w') as file:
 #         file.write("#")
