48c930240e11070ce1d625c854bc437812882849
iqboljon7 <rsfordwyn@gmail.com>
2025-05-13 15:50:29 +0500

    fixed

--- lesson-4/homework/task3.py
+++ lesson-4/homework/task3.py
@@ -1,20 +1,12 @@
 txt = input()
-vowels = 'aeiou'
-i = 2 
-result = ''
+vowels = "aeiou"
+i = 2
+used_letters = ""
 while i < len(txt):
-    result += txt[:i + 1]
-    if i + 1 < len(txt):
-        next_char = txt[i + 1]
-        if next_char in vowels or next_char == '_': # Add the next character too and then underscore
-            result += next_char + '_'
-            txt = txt[i + 2:]  # Remove processed part
-            i = 2  # Reset index for new substring
-            continue
-        else:
-            result += '_'
-    txt = txt[i + 1:]  # Remove processed part
-    i = 2  # Reset index for new substring
-
-result += txt  # Add the rest of the string
+    if txt[i] not in vowels and txt[i] not in used_letters and i != len(txt) - 1:
+        txt = txt[: i + 1] + "_" + txt[i + 1 :]
+        used_letters += txt[i]
+        i += 4
+    else:
+        i += 1
 print(txt)
